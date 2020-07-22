699af6bfe4e50c7c490d532183dc31a60af93b81
MollyKim <kimmh3876@gmail.com>
2020-07-22 17:13:07 +0900

    complete
    I have learn how to connect firestore.
    Also in update situation, two users can access at the same time and it cause differency which is called concurrncy failure. so we need to use increament(1), or we can use async.

--- flutter_firestore/lib/main.dart
+++ flutter_firestore/lib/main.dart
@@ -1,15 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
-
-final dummySnapshot = [
-  {"name": "Filip", "votes": 15},
-  {"name": "A", "votes": 14},
-  {"name": "BB", "votes": 11},
-  {"name": "CCC", "votes": 1},
-  {"name": "DDDD", "votes": 10},
-];
 
 class MyApp extends StatelessWidget {
   @override
@@ -23,7 +15,9 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() {
+    return _MyHomePageState();
+  }
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -34,37 +28,44 @@ class _MyHomePageState extends State<MyHomePage> {
       body: _buildBody(context),
     );
   }
-}
 
-Widget _buildBody(BuildContext context) {
-  return _buildList(context, dummySnapshot);
-}
+  Widget _buildBody(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('DB').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
 
-Widget _buildList(BuildContext context, List<Map> snapshot) {
-  return ListView(
-    padding: const EdgeInsets.only(top: 20.0),
-    children: snapshot.map((data) => _buildListItem(context, data)).toList(),
-  );
-}
+        return _buildList(context, snapshot.data.documents);
+      },
+    );
+  }
 
-Widget _buildListItem(BuildContext context, Map data) {
-  final record = Record.fromMap(data);
+  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
 
-  return Padding(
-    key: ValueKey(record.name),
-    padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-    child: Container(
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey),
-        borderRadius: BorderRadius.circular(5.0),
+  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+    final record = Record.fromSnapshot(data);
+
+    return Padding(
+      key: ValueKey(record.name),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: ListTile(
+            title: Text(record.name),
+            trailing: Text(record.votes.toString()),
+            onTap: () => record.reference
+                .updateData({'votes': FieldValue.increment(1)})),
       ),
-      child: ListTile(
-        title: Text(record.name),
-        trailing: Text(record.votes.toString()),
-        onTap: () => print(record),
-      ),
-    ),
-  );
+    );
+  }
 }
 
 class Record {
